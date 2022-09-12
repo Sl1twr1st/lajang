c047ddab9d406f728ac98e0504166c4b81a3d42a
rizali alma k <jali@rizalis-MacBook-Pro-2.local>
2022-09-12 19:13:20 +0700

    update12sept2022

--- components/toolbar.js
+++ components/toolbar.js
@@ -1,14 +1,24 @@
-import { useRouter } from 'next/router';
-import styles from '../styles/Toolbar.module.css';
+import { useRouter } from "next/router";
+import styles from "../styles/Toolbar.module.css";
 
 export const Toolbar = () => {
   const router = useRouter();
 
   return (
     <div className={styles.main}>
-      <div onClick={() => router.push('/')}>Home</div>
-      <div onClick={() => window.location.href = 'https://singleladies.vercel.app/'}>Mint NFT</div>
-      <div onClick={() => window.location.href = 'https://github.com/Sl1twr1st'}>GitHub</div>
+      <div onClick={() => router.push("/")}>Home</div>
+      <div
+        onClick={() =>
+          (window.location.href = "https://singleladies.vercel.app/")
+        }
+      >
+        Mint NFT
+      </div>
+      <div
+        onClick={() => (window.location.href = "https://github.com/Sl1twr1st")}
+      >
+        Blog
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { Toolbar } from '../components/toolbar';
-import imageUrlBuilder from '@sanity/image-url';
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { Toolbar } from "../components/toolbar";
+import imageUrlBuilder from "@sanity/image-url";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Home({ posts }) {
   const router = useRouter();
@@ -12,16 +12,16 @@ export default function Home({ posts }) {
   useEffect(() => {
     if (posts.length) {
       const imgBuilder = imageUrlBuilder({
-        projectId: 'w0gma1wd',
-        dataset: 'production',
+        projectId: "w0gma1wd",
+        dataset: "production",
       });
 
       setMappedPosts(
-        posts.map(p => {
+        posts.map((p) => {
           return {
             ...p,
             mainImage: imgBuilder.image(p.mainImage).width(500).height(250),
-          }
+          };
         })
       );
     } else {
@@ -33,39 +33,47 @@ export default function Home({ posts }) {
     <div>
       <Toolbar />
       <div className={styles.main}>
-        <h1>Welcome To Parasitlajang2.3</h1>
+        <h1>Welcome To Parasitlajang2.3 Blog</h1>
 
         <h3>Recent Posts:</h3>
 
         <div className={styles.feed}>
-          {mappedPosts.length ? mappedPosts.map((p, index) => (
-            <div onClick={() => router.push(`/post/${p.slug.current}`)} key={index} className={styles.post}>
-              <h3>{p.title}</h3>
-              <img className={styles.mainImage} src={p.mainImage} />
-            </div>
-          )) : <>No Posts Yet</>}
+          {mappedPosts.length ? (
+            mappedPosts.map((p, index) => (
+              <div
+                onClick={() => router.push(`/post/${p.slug.current}`)}
+                key={index}
+                className={styles.post}
+              >
+                <h3>{p.title}</h3>
+                <img className={styles.mainImage} src={p.mainImage} />
+              </div>
+            ))
+          ) : (
+            <>No Posts Yet</>
+          )}
         </div>
       </div>
     </div>
   );
 }
 
-export const getServerSideProps = async pageContext => {
+export const getServerSideProps = async (pageContext) => {
   const query = encodeURIComponent('*[ _type == "post" ]');
   const url = `https://w0gma1wd.api.sanity.io/v1/data/query/production?query=${query}`;
-  const result = await fetch(url).then(res => res.json());
+  const result = await fetch(url).then((res) => res.json());
 
   if (!result.result || !result.result.length) {
     return {
       props: {
         posts: [],
-      }
-    }
+      },
+    };
   } else {
     return {
       props: {
         posts: result.result,
-      }
-    }
+      },
+    };
   }
 };
